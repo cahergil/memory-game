2b7b76ebbaf11ef6baf23aac7e0e54c681f61115
Carlos Hernández Gil <chernandezgil@gmail.com>
2018-07-06 12:55:13 +0200

    refactor: Refactoring css udacity style guide

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ function add() {
   timeTag.innerHTML =  (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
   ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
   ":" + (seconds > 9 ? seconds : "0" + seconds);
-  console.log(timeTag.textContext);
+  //console.log(timeTag.textContext);
 
 }
 
